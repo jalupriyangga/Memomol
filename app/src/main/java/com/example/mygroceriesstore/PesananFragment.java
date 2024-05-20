f442651842479a0aeec58b17b74a0b6da492700f
Theoraditya <theoraditya195@gmail.com>
2024-05-20 17:07:21 +0700

    update teyo

--- app/src/main/java/com/example/mygroceriesstore/PesananFragment.java
+++ app/src/main/java/com/example/mygroceriesstore/PesananFragment.java
@@ -59,6 +59,6 @@ public class PesananFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pesanan, container, false);
+        return inflater.inflate(R.layout.fragment_order, container, false);
     }
 }
---
+++ app/src/main/java/com/example/mygroceriesstore/activities/OrderActivity.java
@@ -0,0 +1,87 @@
+package com.example.mygroceriesstore.activities;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mygroceriesstore.MyCartsFragment;
+import com.example.mygroceriesstore.R;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerView;
+    private MyCartsFragment MycartsFragment;
+    private TextView totalFeeTxt, taxTxt, deliveryTxt, totalTxt, emptyTxt;
+    private double tax;
+    private ScrollView scrollView;
+    private ImageView backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        MycartsFragment = new MyCartsFragment(this);
+
+        initView();
+        setVariavle();
+        initList();
+        calcualteCart();
+    }
+
+    private void initList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new CartListAdapter(managmentCart.getListCart(), this, new ChangeNumberItemsListener() {
+            @Override
+            public void change() {
+                calcualteCart();
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+        if (managmentCart.getListCart().isEmpty()) {
+            emptyTxt.setVisibility(View.VISIBLE);
+            scrollView.setVisibility(View.GONE);
+        } else {
+            emptyTxt.setVisibility(View.GONE);
+            scrollView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void calcualteCart() {
+        double percentTax = 0.02;  //you can change this value for tax price
+        double delivery = 10;
+        tax = Math.round((managmentCart.getTotalFee() * percentTax * 100.0)) / 100.0;
+
+        double total = Math.round((managmentCart.getTotalFee() + tax + delivery) * 100) / 100;
+        double itemTotal = Math.round(managmentCart.getTotalFee() * 100) / 100;
+
+        totalFeeTxt.setText("$" + itemTotal);
+        taxTxt.setText("$" + tax);
+        deliveryTxt.setText("$" + delivery);
+        totalTxt.setText("$" + total);
+    }
+
+    private void setVariavle() {
+        backBtn.setOnClickListener(v -> finish());
+    }
+
+    private void initView() {
+        totalFeeTxt = findViewById(R.id.totalFeeTxt);
+        taxTxt = findViewById(R.id.taxTxt);
+        deliveryTxt = findViewById(R.id.deliveryTxt);
+        totalTxt = findViewById(R.id.totalTxt);
+        recyclerView = findViewById(R.id.view3);
+        scrollView = findViewById(R.id.scrollView2);
+        backBtn = findViewById(R.id.backBtn);
+        emptyTxt = findViewById(R.id.emptyTxt);
+    }
+}
